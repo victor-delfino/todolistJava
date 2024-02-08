75c259b1540be85d41d91ba078578c27a51ab2b3
victor-delfino <hugo.delfino11@gmail.com>
2024-02-08 16:33:48 -0300

    Criação do Service da aplicação

---
+++ todolist/src/main/java/br/com/victordelfino/todolist/service/TodoService.java
@@ -0,0 +1,34 @@
+package br.com.victordelfino.todolist.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import br.com.victordelfino.todolist.entity.Todo;
+import br.com.victordelfino.todolist.repository.TodoRepository;
+
+@Service
+public class TodoService {
+    private TodoRepository todoRepository;
+    
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+    public List<Todo> create(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+    public List<Todo> list() {
+       Sort sort =  Sort.by("prioridade").descending().and(Sort.by("nome")).ascending();
+        return todoRepository.findAll(sort);
+    }
+    public List<Todo> update(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+    public List<Todo> delete(Long id) {
+        todoRepository.deleteById(id);
+        return list();
+    }
+}
